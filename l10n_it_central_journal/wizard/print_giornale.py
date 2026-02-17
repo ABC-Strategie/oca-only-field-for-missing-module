0995a60392cd3efa35c1d9e5a1b8643bdc9f3a91
masimassimo <dr.masi.massimo@gmail.com>
2026-02-17 10:20:56 +0100

    Update print_giornale.py

--- l10n_it_central_journal/wizard/print_giornale.py
+++ l10n_it_central_journal/wizard/print_giornale.py
@@ -3,7 +3,7 @@
 from datetime import timedelta
 
 from odoo import _, api, fields, models
-from odoo.exceptions import Warning as UserError
+from odoo.exceptions import UserError
 from odoo.tools.misc import flatten
 
 
