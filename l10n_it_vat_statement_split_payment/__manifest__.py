325dacb0e7dc0bc838badf8ae498cb84b01d5909
masimassimo <dr.masi.massimo@gmail.com>
2026-04-02 17:10:54 +0200

    Update __manifest__.py

--- l10n_it_vat_statement_split_payment/__manifest__.py
+++ l10n_it_vat_statement_split_payment/__manifest__.py
@@ -17,7 +17,7 @@
     "application": False,
     "installable": True,
     "auto_install": True,
-    "depends": ["account_vat_period_end_statement", "l10n_it_split_payment"],
+    "depends": ["l10n_it_account_vat_period_end_statement", "l10n_it_split_payment"],
     "data": [
         # "views/account_config_view.xml"
     ],
