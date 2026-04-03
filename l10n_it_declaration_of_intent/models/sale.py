bf9897a9e46249595bb7a80665ac34de57311640
masimassimo <dr.masi.massimo@gmail.com>
2026-04-03 12:34:42 +0200

    Update sale.py

--- l10n_it_declaration_of_intent/models/sale.py
+++ l10n_it_declaration_of_intent/models/sale.py
@@ -7,22 +7,22 @@ from odoo import api, fields, models
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    def _set_fiscal_position(self):
-        for sale in self:
-            if sale.partner_id and sale.date_order:
-                declarations = self.env[
-                    "l10n_it_declaration_of_intent.declaration"
-                ].get_valid(
-                    "out", sale.partner_id.id, fields.Date.to_date(sale.date_order)
-                )
-                if declarations:
-                    sale.fiscal_position_id = declarations[0].fiscal_position_id.id
+    # def _set_fiscal_position(self):
+    #     for sale in self:
+    #         if sale.partner_id and sale.date_order:
+    #             declarations = self.env[
+    #                 "l10n_it_declaration_of_intent.declaration"
+    #             ].get_valid(
+    #                 "out", sale.partner_id.id, fields.Date.to_date(sale.date_order)
+    #             )
+    #             if declarations:
+    #                 sale.fiscal_position_id = declarations[0].fiscal_position_id.id
 
-    @api.onchange("date_order")
-    def onchange_date_order(self):
-        self._set_fiscal_position()
+    # @api.onchange("date_order")
+    # def onchange_date_order(self):
+    #     self._set_fiscal_position()
 
-    def _compute_fiscal_position_id(self):
-        res = super()._compute_fiscal_position_id()
-        self._set_fiscal_position()
-        return res
+    # def _compute_fiscal_position_id(self):
+    #     res = super()._compute_fiscal_position_id()
+    #     self._set_fiscal_position()
+    #     return res
